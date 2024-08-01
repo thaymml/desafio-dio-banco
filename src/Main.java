b9291de4647dae8e6567778a4be03be95b89efb9
Thayssa <thayssamml@outlook.com>
2024-08-01 14:49:16 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Cliente thay = new Cliente();
+        thay.setNome("Thay");
+
+        Conta cc = new ContaCorrente(thay);
+        Conta poupanca = new ContaPoupanca(thay);
+
+        cc.depositar(100);
+        cc.transferir(100, poupanca);
+
+        cc.imprimirExtrato();
+        poupanca.imprimirExtrato();
+    }
+}
